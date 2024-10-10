59ff16e8b417caa928b20146e98f3250b8d42b22
TheLostone145 <martinthegamer145@gmail.com>
2024-10-10 13:15:21 -0400

    Fixed and created Button and implemented label

--- MenuScreen.java
+++ MenuScreen.java
@@ -8,14 +8,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MenuScreen extends World
 {
-
-    /**
-     * Constructor for objects of class MenuStart.
-     * 
-     */
+    Label button;
+    
     public MenuScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        button=new Label(0,80);
     }
 }
---
+++ Queue.java
@@ -0,0 +1,22 @@
+public class Queue {
+    public class QueueOfStrings{
+        private Node first, last;
+        private class Node{ String item; Node next; }
+        public boolean isEmpty() { return (first == null); }
+        public void enqueue(String item) {
+            Node oldLast = last;
+            last = new Node();
+            last.item = item;
+            last.next = null;
+            if (isEmpty())  first = last;
+            else            oldLast.next = last;
+        }
+        public String dequeue() {
+            String item = first.item;
+            first = first.next;
+            if (isEmpty())  first = last;
+            return item;
+        }
+    }
+    
+}
