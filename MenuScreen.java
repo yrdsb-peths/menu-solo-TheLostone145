5358601c528b4624b2f0a535bfe8db1c413186d9
TheLostone145 <martinthegamer145@gmail.com>
2024-10-15 13:14:05 -0400

    Menu screen

--- MenuScreen.java
+++ MenuScreen.java
@@ -3,8 +3,12 @@ import greenfoot.*;
 public class MenuScreen extends World {
   public MenuScreen() {
     super(600, 400, 1);
-    Label button =new Label("images/buttonLong_beige.png",100);
-    addObject(button, 300, 340);
+
+    addObject(new Button(this::goInstructions), 300, 340);
+  }
+
+  public void goInstructions() {
+    Greenfoot.setWorld(new InstructionScreen(this));
   }
 
 }
