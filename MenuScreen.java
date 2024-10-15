6df8b05d767d33602276f95e120454c34b1acdbc
TheLostone145 <martinthegamer145@gmail.com>
2024-10-15 13:01:56 -0400

    Menuscreen Part 2 Electric Boogaloo

--- MenuScreen.java
+++ MenuScreen.java
@@ -3,12 +3,12 @@ import greenfoot.*;
 public class MenuScreen extends World {
   public MenuScreen() {
     super(600, 400, 1);
-    addObject(new Button, 300, 340);
-    //addObject(new Button(this::goInstructions), 300, 340);
+
+    addObject(new Button(this::goInstructions), 300, 340);
   }
 
- // public void goInstructions() {
- //   Greenfoot.setWorld(new InstructionScreen(this));
- // }
+  public void goInstructions() {
+    Greenfoot.setWorld(new InstructionScreen(this));
+  }
 
 }
