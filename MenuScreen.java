cb526a5c5ce737cd9e5f730c8e12bed48f927a3c
TheLostone145 <martinthegamer145@gmail.com>
2024-10-16 11:52:12 -0400

    Added Screen cycling

--- MenuScreen.java
+++ MenuScreen.java
@@ -2,26 +2,28 @@ import greenfoot.*;
 
 public class MenuScreen extends World {
     private Queue<GreenfootImage> avatarQueue;
+    private Queue<Label> screenQueue;
     private GreenfootImage currentAvatar;
+    private Label currentScreen;
     private Label avatarLabel;
+    private Label screenLabel;
 
     public MenuScreen() {
         super(600, 400, 1);
 
-        addObject(new Button(this::goInstructions), 300, 350);
+        addObject(new Button(this::cycleScreens), 300, 350);
         
         avatarQueue = new Queue<>();
         loadAvatars();
+        loadScreen();
         currentAvatar = avatarQueue.peek();
+        currentScreen =screenQueue.peek();
         avatarLabel = new Label(currentAvatar, 100, 100);
+        screenLabel = new Label(currentScreen, 100, 100);
         addObject(avatarLabel, 300, 200);
+        addObject(screenLabel, 300, 200);
         
-       
         addObject(new Button(this::cycleAvatar), 300, 300);
-    }
-
-    public void goInstructions() {
-        Greenfoot.setWorld(new InstructionScreen(this));
     }
 
     private void loadAvatars() {
@@ -34,6 +36,18 @@ public class MenuScreen extends World {
         GreenfootImage firstAvatar = avatarQueue.dequeue();
         avatarQueue.enqueue(firstAvatar);
         currentAvatar = avatarQueue.peek();
-        avatarLabel.setImage(currentAvatar);  // Assuming Label can handle an image
+        avatarLabel.setImage(currentAvatar); 
+    }
+    private void loadScreen() {
+        screenQueue.enqueue(new Label(1, 100));
+        screenQueue.enqueue(new Label(2, 100));
+        screenQueue.enqueue(new Label(3, 100));
+    }
+
+    public void cycleScreens() {
+        Label firstScreen= screenQueue.dequeue();
+        screenQueue.enqueue(firstScreen);
+        currentScreen = screenQueue.peek();
+        screenLabel=currentScreen; 
     }
 }
