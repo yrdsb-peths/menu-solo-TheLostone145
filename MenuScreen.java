26c1ea71868137bab1d5253bf3696cfa7fd0bace
TheLostone145 <martinthegamer145@gmail.com>
2024-10-16 11:29:19 -0400

    Did Menu

--- MenuScreen.java
+++ MenuScreen.java
@@ -1,14 +1,39 @@
 import greenfoot.*;
 
 public class MenuScreen extends World {
-  public MenuScreen() {
-    super(600, 400, 1);
+    private Queue<GreenfootImage> avatarQueue;
+    private GreenfootImage currentAvatar;
+    private Label avatarLabel;
 
-    addObject(new Button(this::goInstructions), 300, 340);
-  }
+    public MenuScreen() {
+        super(600, 400, 1);
 
-  public void goInstructions() {
-    Greenfoot.setWorld(new InstructionScreen(this));
-  }
+        addObject(new Button(this::goInstructions), 300, 300);
+        
+        avatarQueue = new Queue<>();
+        loadAvatars();
+        currentAvatar = avatarQueue.peek();
+        avatarLabel = new Label(currentAvatar, 100, 100);
+        addObject(avatarLabel, 300, 200);
+        
+        // Cycle Avatar Button
+        addObject(new Button(this::cycleAvatar), 300, 350);
+    }
 
+    public void goInstructions() {
+        Greenfoot.setWorld(new InstructionScreen(this));
+    }
+
+    private void loadAvatars() {
+        avatarQueue.enqueue(new GreenfootImage("images/face_a.png"));
+        avatarQueue.enqueue(new GreenfootImage("images/face_b.png"));
+        avatarQueue.enqueue(new GreenfootImage("images/face_c.png"));
+    }
+
+    public void cycleAvatar() {
+        GreenfootImage firstAvatar = avatarQueue.dequeue();
+        avatarQueue.enqueue(firstAvatar);
+        currentAvatar = avatarQueue.peek();
+        avatarLabel.setImage(currentAvatar);  // Assuming Label can handle an image
+    }
 }
--- Queue.java
+++ Queue.java
@@ -1,22 +1,40 @@
-public class Queue {
-    public class QueueOfStrings{
-        private Node first, last;
-        private class Node{ String item; Node next; }
-        public boolean isEmpty() { return (first == null); }
-        public void enqueue(String item) {
-            Node oldLast = last;
-            last = new Node();
-            last.item = item;
-            last.next = null;
-            if (isEmpty())  first = last;
-            else            oldLast.next = last;
-        }
-        public String dequeue() {
-            String item = first.item;
-            first = first.next;
-            if (isEmpty())  first = last;
-            return item;
-        }
+public class Queue<T> {  
+    private Node first, last;
+
+   
+    private class Node {
+        T item;  
+        Node next;
     }
+
     
+    public boolean isEmpty() { 
+        return (first == null); 
+    }
+
+    
+    public void enqueue(T item) {
+        Node oldLast = last;
+        last = new Node();
+        last.item = item;  
+        last.next = null;
+        if (isEmpty())  
+            first = last;
+        else            
+            oldLast.next = last;
+    }
+
+    
+    public T dequeue() {
+        T item = first.item;  
+        first = first.next;
+        if (isEmpty())  
+            last = null;  
+        return item;
+    }
+
+    public T peek() {
+        if (isEmpty()) return null; 
+        return first.item;  
+    }
 }
