80c178cd53d05834df6fd15a53cae6e87230a3c0
TheLostone145 <martinthegamer145@gmail.com>
2024-10-17 22:12:42 -0400

    Fixed nullpoint exception

--- MenuScreen.java
+++ MenuScreen.java
@@ -2,7 +2,7 @@ import greenfoot.*;
 
 public class MenuScreen extends World {
     private Queue<GreenfootImage> avatarQueue;
-    private Queue<Label> screenQueue;
+    private Queue<Label> screenQueue; 
     private GreenfootImage currentAvatar;
     private Label currentScreen;
     private Label avatarLabel;
@@ -11,18 +11,23 @@ public class MenuScreen extends World {
     public MenuScreen() {
         super(600, 400, 1);
 
-        addObject(new Button(this::cycleScreens), 300, 350);
         
         avatarQueue = new Queue<>();
+        screenQueue = new Queue<>(); 
+
+        addObject(new Button(this::cycleScreens), 300, 350);
+       
         loadAvatars();
-        loadScreen();
+        loadScreen(); 
         currentAvatar = avatarQueue.peek();
-        currentScreen =screenQueue.peek();
+        currentScreen = screenQueue.peek();
+       
         avatarLabel = new Label(currentAvatar, 100, 100);
         screenLabel = new Label(currentScreen, 100, 100);
+       
         addObject(avatarLabel, 300, 200);
         addObject(screenLabel, 300, 200);
-        
+       
         addObject(new Button(this::cycleAvatar), 300, 300);
     }
 
@@ -32,20 +37,21 @@ public class MenuScreen extends World {
         avatarQueue.enqueue(new GreenfootImage("images/face_c.png"));
     }
 
-    public void cycleAvatar() {
-        GreenfootImage firstAvatar = avatarQueue.dequeue();
-        avatarQueue.enqueue(firstAvatar);
-        currentAvatar = avatarQueue.peek();
-        avatarLabel.setImage(currentAvatar); 
-    }
     private void loadScreen() {
         screenQueue.enqueue(new Label(1, 100));
         screenQueue.enqueue(new Label(2, 100));
         screenQueue.enqueue(new Label(3, 100));
     }
 
+    public void cycleAvatar() {
+        GreenfootImage firstAvatar = avatarQueue.dequeue();
+        avatarQueue.enqueue(firstAvatar);
+        currentAvatar = avatarQueue.peek();
+        avatarLabel.setImage(currentAvatar);
+    }
+
     public void cycleScreens() {
-        Label firstScreen= screenQueue.dequeue();
+        Label firstScreen = screenQueue.dequeue();
         screenQueue.enqueue(firstScreen);
         currentScreen = screenQueue.peek();
         screenLabel=currentScreen; 
