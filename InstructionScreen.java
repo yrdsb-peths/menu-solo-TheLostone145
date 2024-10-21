228c8569390b528a9aef3bbe601eaf7a539d471f
TheLostone145 <martinthegamer145@gmail.com>
2024-10-21 19:52:24 -0400

    Removed queues from InstructionsScreen

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -3,9 +3,6 @@ import greenfoot.*;
 public class InstructionScreen extends World 
 {
     private MenuScreen menuScreen;  
-    private Queue<Label> screenQueue; 
-    private Label currentScreen;
-    private Label screenLabel;
     private String[] instructions = {
         "Welcome to the game!",
         "Use arrow keys to move.",
@@ -17,35 +14,12 @@ public class InstructionScreen extends World
     {
         super(600, 400, 1);
         instructionLabel = new Label(instructions[currentIndex], 24);
-        addObject(instructionLabel, 300, 200);
-        screenQueue = new Queue<>(); 
+        addObject(instructionLabel, 300, 200); 
         addObject(new Button(this::goBack), 300, 300);
-        addObject(new Button(this::cycleScreens), 300, 350);
-
-        loadScreen();
-        currentScreen = screenQueue.peek();
-        screenLabel = new Label(currentScreen, 100, 100);
-        addObject(screenLabel, 300, 200);
     }
 
     public void goBack() 
     {
         Greenfoot.setWorld(menuScreen);  
-    }
-
-    private void loadScreen() {
-        screenQueue.enqueue(new Label(instructions[1], 100));
-        screenQueue.enqueue(new Label(instructions[2], 100));
-        screenQueue.enqueue(new Label(instructions[3], 100));
-    }
-    public void cycleScreens() {
-       
-        Label firstScreen = screenQueue.dequeue();
-        screenQueue.enqueue(firstScreen);
-        currentScreen = screenQueue.peek();
-        removeObject(screenLabel);
-        screenLabel = null; 
-        screenLabel = currentScreen;
-        addObject(screenLabel, 300, 200);  
     }
 }
