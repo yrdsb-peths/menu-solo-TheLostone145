f04c8b04545c488a81d87d15df103eab473a51ed
TheLostone145 <martinthegamer145@gmail.com>
2024-10-21 20:25:09 -0400

    Revert "Added buttons so you can scroll through instructions"
    This reverts commit a10ca87ba86c96028b007001a2eb04dd7b5369f7.

--- HighScoreScreen.java
+++ HighScoreScreen.java
@@ -5,26 +5,13 @@ import greenfoot.*;
 public class HighScoreScreen extends World {
     private Map<String, Integer> highScores;
     private MenuScreen menuScreen;
-    
-    private String[] instructions = {
-        "Screen 1",
-        "Screen 2",
-        "Screen 3"
-    };
-    private int currentInstructionIndex = 0;
-    private Label instructionLabel;
-
     public HighScoreScreen(MenuScreen menuScreen) {
         super(600, 400, 1);
         this.menuScreen = menuScreen;
         highScores = new HashMap<>();
         loadScores();
         displayScores();
-        displayInstruction();
-        
         addObject(new Button(this::goBack), 300, 350);
-        addObject(new Button(this::previousInstruction), 150, 350);
-        addObject(new Button(this::nextInstruction), 450, 350);
     }
 
     private void loadScores() {
@@ -41,32 +28,8 @@ public class HighScoreScreen extends World {
             y += 50;
         }
     }
-
-    private void displayInstruction() {
-        if (instructionLabel != null) {
-            removeObject(instructionLabel);
-        }
-        instructionLabel = new Label(instructions[currentInstructionIndex], 24);
-        addObject(instructionLabel, 300, 50);
-    }
-
-    public void previousInstruction() {
-        currentInstructionIndex--;
-        if (currentInstructionIndex < 0) {
-            currentInstructionIndex = instructions.length - 1;
-        }
-        displayInstruction();
-    }
-
-    public void nextInstruction() {
-        currentInstructionIndex++;
-        if (currentInstructionIndex >= instructions.length) {
-            currentInstructionIndex = 0;
-        }
-        displayInstruction();
-    }
-
-    public void goBack() {
+    public void goBack() 
+    {
         Greenfoot.setWorld(menuScreen);  
     }
 }
